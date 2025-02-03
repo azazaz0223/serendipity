af1d587a61e82a5dde1065cf7fb7c6e29b78da3c
azazaz0223 <ray0961331980@gmail.com>
2025-02-03 09:49:30 +0800

    feat: 評估單修改為預設選擇我不確定

--- resources/views/frontend/evaluationForm/variety.blade.php
+++ resources/views/frontend/evaluationForm/variety.blade.php
@@ -77,7 +77,7 @@
                                                     <div
                                                         class="mcb-column-inner mcb-column-inner-361ed5660 mcb-item-column-inner">
                                                         <div class="column_attr mfn-inline-editor clearfix">
-                                                            <div class="tooth active" data-dentition-problem-option="1">
+                                                            <div class="tooth" data-dentition-problem-option="1">
                                                                 <img class="scale-with-grid"
                                                                     src="{{ asset('images/frontend/icon/tooth1.svg') }}">
                                                             </div>
@@ -181,7 +181,7 @@
                                                     <div
                                                         class="mcb-column-inner mcb-column-inner-80426864c mcb-item-column-inner">
                                                         <div class="column_attr mfn-inline-editor clearfix">
-                                                            <div class="tooth" data-dentition-problem-option="7">
+                                                            <div class="tooth active" data-dentition-problem-option="7">
                                                                 <img class="scale-with-grid"
                                                                     src="{{ asset('images/frontend/icon/tooth9.png') }}">
                                                             </div>
